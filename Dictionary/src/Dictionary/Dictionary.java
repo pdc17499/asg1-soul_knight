9ce862a6738f4a48441c62211dd1ca2831c7c82d
duongtuanphuong <43140650+duongtuanphuong@users.noreply.github.com>
2018-10-12 21:17:38 +0700

    Commit

--- Dictionary/src/Dictionary/Dictionary.java
+++ Dictionary/src/Dictionary/Dictionary.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class Dictionary {
     public ArrayList<Word> arr =new ArrayList<>();
+    public String toString(int vitri,int dem){
+        Word s=arr.get(vitri);
+        return "    |"+dem+"    |"+s.getWord_target()+"          |"+s.getWord_explain();
+    }
+
 
 
 }
--- Dictionary/src/Dictionary/DictionaryManagement.java
+++ Dictionary/src/Dictionary/DictionaryManagement.java
@@ -1,19 +1,27 @@
 package Dictionary;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DictionaryManagement {
     Dictionary dic= new Dictionary();
     public void insertFromCommandline(){
         Scanner sc = new Scanner(System.in);
+        int s1;
+        System.out.println("Nhập số từ muốn thêm :");
+        s1=sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<s1;i++){
+            Word word =new Word();
+            System.out.println("Tiếng Anh");
+            word.setWord_target(sc.nextLine());
+            System.out.println("Tiếng Việt");
+            word.setWord_explain(sc.nextLine());
+            dic.arr.add(word);
 
-        Word word =new Word();
-        System.out.println("Tiếng Anh");
-        word.setWord_target(sc.nextLine());
-        System.out.println("Tiếng Việt");
-        word.setWord_explain(sc.nextLine());
-        dic.arr.add(word);
+        }
+
 
     }
 
@@ -38,8 +46,35 @@ public class DictionaryManagement {
 
     }
     public void dictionaryLookup(){
+        System.out.println("Nhập từ muốn tìm kiếm :");
+        String a= new String();
+        Scanner sc =new Scanner(System.in);
+        a=sc.nextLine();
+        int dem =0;
+        for (int i=0;i<dic.arr.size();i++){
+            if(dic.arr.get(i).getWord_target().contains(a)){
+                dem ++;
+                System.out.println(dic.arr.get(i).getWord_target() + " "+ dic.arr.get(i).getWord_explain());
+            }
+            else if (dem==0){
+                System.out.println("Từ bạn nhập ko được tìm thấy");
+            }
+        }
+    }
+    public void removeWord(){
+        System.out.println("Mời nhập từ cần xóa");
+        String b=new String();
+        Scanner sc =new Scanner(System.in);
+        b=sc.nextLine();
+        int dem=0;
+        for (int i=0;i<dic.arr.size();i++){
+            if (dic.arr.get(i).getWord_target().contains(b)){
+                dem++;
+                dic.arr.remove(dic.arr.get(i));
 
-
+            }
+            else if(dem==0) System.out.println("Từ bạn nhập ko có trong từ điển");
+        }
     }
     public void dictionaryExportToFile(){
 
