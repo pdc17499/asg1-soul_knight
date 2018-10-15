36de40ff42729c6fcfc29de95aec52f9d1759db7
pdc17499 <43161538+pdc17499@users.noreply.github.com>
2018-10-15 13:08:01 +0700

    1.3

--- Dictionary/src/Dictionary/DictionaryCommandline.java
+++ Dictionary/src/Dictionary/DictionaryCommandline.java
@@ -1,15 +1,20 @@
 package Dictionary;
 
+
 import java.util.ArrayList;
 
-
 public class DictionaryCommandline {
-    DictionaryManagement dictionaryManagement = new DictionaryManagement();
-    public void showAllWords(){
+
+    public void showAllWords(Dictionary k){
+        ArrayList<Word>arr=new ArrayList<>();
+         arr= k.getDictionary();
+            for (int i = 1; i < arr.size(); i++) {
+                System.out.println(i + "     |" + arr.get(i).getWord_target() + "    |"+ arr.get(i).getWord_explain());
+            }
+        }
 
 
 
-    }
     public void dictionaryBasic(){
 
     }
