03b5ca14e081102c69a81b84f4bb546f418f8aeb
duongtuanphuong <43140650+duongtuanphuong@users.noreply.github.com>
2018-10-20 16:08:56 +0700

    Update Word.java
    sửa lỗi

--- Dictionary/src/Dictionary/Word.java
+++ Dictionary/src/Dictionary/Word.java
@@ -1,7 +1,7 @@
 package Dictionary;
 
 public class Word {
-    private String word_target,word_explain;
+     String word_target,word_explain;
 
     public void setWord_target(String word_target) {
         this.word_target = word_target;
@@ -15,5 +15,12 @@ public class Word {
     }
     public String getWord_explain() {
         return word_explain;
+    }
+    public Word(String word_target, String word_explain){
+        this.word_target = word_target;
+        this.word_explain=word_explain;
+    }
+    public Word(){
+
     }
 }
